10ed6a78df0a2b7b84a7e3756d49be53639e4e51
bharath <chinnabalu590@gmail.com>
2022-03-25 23:25:00 -0500

    adding text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is bharath
+          This is bharath1
         </p>
         <a
           className="App-link"
